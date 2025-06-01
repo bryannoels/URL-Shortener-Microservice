2632d6755f271321b5194317a47cff9a86dab7fb
bryannoels <bryannoelsalindeho23@gmail.com>
2025-06-01 20:25:10 +0700

    feat: create the url shorterner controller

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+let urlDatabase = []
+let id = 1
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -21,4 +26,43 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+//POST endpoint to create a new URL
+app.post('/api/shorturl', (req, res) => {
+  let originalUrl = req.body.url;
+  try {
+    let urlObj = new URL(originalUrl);
+    dns.lookup(urlObj.hostname, (err) => {
+      if (err) {
+        console.log('DNS lookup error:', err);
+        return res.json({ "error": 'invalid url' });
+      }
+      let found = urlDatabase.find(url => url.original_url === originalUrl);
+      if (found) {
+        return res.json(found);
+      } else {
+        let newUrl = {
+          original_url: originalUrl,
+          short_url: id++
+        };
+        urlDatabase.push(newUrl);
+        return res.json(newUrl);
+      }
+    });
+  } catch (e) {
+    console.log('URL parsing error:', e);
+    return res.json({ "error": 'invalid url' });
+  }
+});
+
+//GET endpoint to redirect to the original URL
+app.get('/api/shorturl/:short_url', (req, res) => {
+  let shortUrl = req.params.short_url;
+  let found = urlDatabase.find(url => url.short_url == shortUrl);
+  if (found) {
+    return res.redirect(found.original_url);
+  } else {
+    return res.json({ "error": 'invalid url' });
+  }
 });
